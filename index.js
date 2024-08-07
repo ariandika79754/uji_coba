0d45e1b6c251e2ad749c57b9ad59a49073a173c6
Ari Andika <ariandika79754@gmail.com>
2024-08-07 14:11:11 +0700

    Update Bonus

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Exercise Variable
 // Deklarasi variabel menggunakan let
 let name = "Ari Andika Leva Pratama";
 let age = 20;
@@ -7,18 +8,20 @@ let citizen = "Desa Rawa Selapan, Kec. Candipuro, Kab. Lamsel, Lampung Indonesia
 // Cetak variabel-variabel tersebut ke console
 console.log(`Nama saya ${name}, saya berusia ${age} tahun, saya dari universitas ${university} dan berasal dari ${citizen}.`);
 
-
+//Exercise Function
 // Mendefinisikan fungsi animal
 function animal(name, type) {
     return `${name} merupakan binatang dengan jenis ${type}`;
 }
 
 // Memanggil fungsi animal dengan argumen
-const result = animal("Ari", "Manusia");
+const result = animal("Anjing", "Hewan Mamalia");
 
 // Mencetak hasilnya ke console
 console.log(result);
 
+
+// Exercise Loop
 // Membuat array berisi lima nama teman
 const friends = ["Adnan", "Angga", "Raka", "Ari", "Metha"];
 
@@ -26,6 +29,9 @@ const friends = ["Adnan", "Angga", "Raka", "Ari", "Metha"];
 for (let i = 0; i < friends.length; i++) {
     console.log(friends[i]);
 }
+
+
+// Exercise Class
 
 // Mendefinisikan class Mahasiswa
 class Mahasiswa {
@@ -46,3 +52,37 @@ const mahasiswa1 = new Mahasiswa("Ari Andika Leva Pratama", "Manajemen Informati
 
 // Memanggil method introduction
 mahasiswa1.introduction();
+
+// BONUS 
+// Mendefinisikan kelas Perpustakaan
+class Perpustakaan {
+    // Constructor untuk menginisialisasi properti books sebagai array kosong
+    constructor() {
+        this.books = [];
+    }
+
+    // Metode addBook untuk menambahkan buku ke dalam array books
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    // Metode showBooks untuk mencetak setiap judul buku yang ada di perpustakaan
+    showBooks() {
+        console.log("Daftar Buku di Perpustakaan:");
+        for (let i = 0; i < this.books.length; i++) {
+            console.log(this.books[i]);
+        }
+    }
+}
+
+// Membuat objek dari kelas Perpustakaan
+const perpustakaan = new Perpustakaan();
+
+// Menambahkan buku ke dalam perpustakaan
+perpustakaan.addBook("Matematika");
+perpustakaan.addBook("Bahasa Inggris");
+perpustakaan.addBook("Pemrograman JavaScript");
+perpustakaan.addBook("Bahasa Indonesia");
+
+// Menampilkan daftar buku di perpustakaan
+perpustakaan.showBooks();
